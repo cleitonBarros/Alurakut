1cfb905ad7777251d38e9e770a67aa24560b9ec9
cleitonBarros <48657479+cleitonBarros@users.noreply.github.com>
2022-11-11 14:53:29 -0300

    commit aula 2

--- alurakut/pages/index.tsx
+++ alurakut/pages/index.tsx
@@ -1,4 +1,4 @@
-import Head from 'next/head'
+
 import React from 'react'
 import MainGrid from '../components/MainGrid'
 import Box from '../components/Box'
@@ -7,7 +7,7 @@ import {ProfileRelationsBoxWrapper} from '../components/profileRalationsArea'
 
 function ProfileSiderBar(Props: any){
   return(
-    <Box>
+    <Box as="aside">
     <img src={`https://github.com/${Props.githubUser}.png`} alt={`imagem de perfil de ${Props.githubUser}`} style={{borderRadius: '8px'}}/>
     <hr />
 
@@ -31,10 +31,7 @@ export default function Home() {
   const pessoasFavoritas = [
     'juunegreiros',
     'omariosouto',
-    'peas',
     'rafaballerini',
-    'marcobrunodev',
-    'felipefialho',
     'maykbrito',
     'Rocketseat',
     'diego3g'
@@ -42,34 +39,34 @@ export default function Home() {
 
   return (
   <>
-  <AlurakutMenu  />
+  <AlurakutMenu githubUser={undefined} />
   <MainGrid>
     <div className="profileArea" style ={{ gridArea: 'profileArea'}}>
       <ProfileSiderBar githubUser={githubUser} /> 
     </div>
     <div className="welcomeArea" style ={{ gridArea: 'welcomeArea'}}>
-      <Box>
+      <Box as="aside">
         <h1 className="title">Bem vindo (a) </h1>
         <OrkutNostalgicIconSet />
       </Box>
       
       <Box>
         <h2 className="subTitle">o que foce deseja fazer</h2>
-        <form onSubmit={function handleCreateCommunit(e){
-          e.preventDefault();
+        <form onSubmit={function handleCriaComunidade(e) {
+                e.preventDefault();
+                const dadosDoForm = new FormData(e.target);
 
-          const dadosDoForm = new FormData(e.target);
+                console.log('Campo: ', dadosDoForm.get('title'));
+                console.log('Campo: ', dadosDoForm.get('image'));
 
-          const comunidade ={
-            id: new Date().toISOString(),
-            title: dadosDoForm.get('title'),
-            image: dadosDoForm.get('image'),
-          }
-
-          const comunidadesAtualizadas = [...comunidades, comunidade]
-          setComunidades(comunidadesAtualizadas)
-
-        }}>
+                const comunidade = {
+                  id: new Date().toISOString(),
+                  title: dadosDoForm.get('title'),
+                  image: dadosDoForm.get('imagem'),
+                }
+                const comunidadesAtualizadas = [...comunidades, comunidade];
+                setComunidades(comunidadesAtualizadas)
+            }}>
           <div>
             <input 
               placeholder="Qual vai ser o nome da sua comunidade?" 
@@ -97,8 +94,8 @@ export default function Home() {
         <ul>
           {pessoasFavoritas.map((item)=>{
             return(
-                <li>
-                  <a href={`/users/${item}`} key={item}>
+                <li key={item}>
+                  <a href={`/users/${item}`} >
                     <img src={`https://github.com/${item}.png`} />
                     <span>{item}</span>
                   </a>
@@ -111,18 +108,20 @@ export default function Home() {
             <h2 className="smallTitle">
               Comunidades ({comunidades.length})
             </h2>
+           
             <ul>
-              {comunidades.map((item) => {
+              {comunidades.map((itemAtual) => {
                 return (
-                  <li>
-                  <a href={`/users/${item.title}`} key={item.title}>
-                    <img src={item.image} />
-                    <span>{item.title}</span>
-                  </a>
-                </li>
+                  <li key={itemAtual.id}>
+                    <a href={`/users/${itemAtual.title}`}>
+                      <img src={itemAtual.image} />
+                      <span>{itemAtual.title}</span>
+                    </a>
+                  </li>
                 )
               })}
             </ul>
+
           </ProfileRelationsBoxWrapper>
     </div>
   </MainGrid>
